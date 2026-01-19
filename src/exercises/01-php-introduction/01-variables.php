8ab3485005a72081ca48b51642d6dbe345e2e6d8
Owen Bilag <N00253477@iadt.ie>
2026-01-19 12:51:34 +0000

    My First Commit

--- src/exercises/01-php-introduction/01-variables.php
+++ src/exercises/01-php-introduction/01-variables.php
@@ -27,6 +27,10 @@
     <div class="output">
         <?php
         // TODO: Write your solution here
+        $firstName = "Owen";
+        $lastName = "Bilag";
+
+        echo "My name is $firstName $lastName";
         ?>
     </div>
 
