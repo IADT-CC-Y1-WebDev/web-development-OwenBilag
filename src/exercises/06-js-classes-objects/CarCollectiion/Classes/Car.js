4789bfaadf7f4ecb1c1b8fdc4a7eb050fdf4c355
Owen Bilag <N00253477@iadt.ie>
2026-03-03 13:14:51 +0000

    car collection p1

---
+++ src/exercises/06-js-classes-objects/CarCollectiion/Classes/Car.js
@@ -0,0 +1,18 @@
+class Car {
+    constructor(_make, _model, _year, _colour){
+        this.make = _make;
+        this.model = _model;
+        this.year = _year;
+        this.colour = _colour;
+    }
+    toString(){
+    return `
+        Make: ${this.make}
+        Model: ${this.model}
+        Year: ${this.year}
+        Colour: ${this.color}
+        `;
+    } 
+};
+
+export default Car;
---
+++ src/exercises/06-js-classes-objects/CarCollectiion/app.js
@@ -0,0 +1,4 @@
+import Car from "/classes/Car.js";
+
+let bmw = new Car('BMW', '5 Series', 2026, 'Green');
+console.log(`${bmw}`);
